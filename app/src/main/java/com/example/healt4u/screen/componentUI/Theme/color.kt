7d178d3fa336912ae65f19b952f73cb5bc9af2cf
kellycpe-pm2 <kellycpe-pm25@student.tarc.edu.my>
2026-08-08 03:05:20 +0800

    ADD UI Component and also Add color file

---
+++ app/src/main/java/com/example/healt4u/screen/componentUI/Theme/color.kt
@@ -0,0 +1,41 @@
+package com.example.healt4u.screen.componentUI.Theme
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputTextFieldTheme(content: @Composable () -> Unit) {
+    val customColors = lightColorScheme(
+        primary = Color(0xFFE9FCFF),
+        onPrimary = Color(0xFF011792),
+        secondary = Color(0xFF3674EE),
+        onSecondary = Color(0xFFFFFFFF),
+        background = Color(0xFFE9FCFF),
+        onBackground = Color(0xFF011792),
+        surface = Color(0xFFFFFFFF),
+        onSurface = Color(0xFF011792),
+        error = Color(0xFFD32F2F),
+        onError = Color(0xFFFFFFFF)
+    )
+
+    val customType = Typography(
+        headlineMedium = TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontSize = 26.sp
+        ),
+        bodyLarge = TextStyle(
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp
+        )
+    )
+
+    MaterialTheme(
+        colorScheme = customColors,
+        typography = customType,
+        content = content
+    )
+}
--- app/src/main/java/com/example/healt4u/screen/componentUI/dataFieldInput.kt
+++ app/src/main/java/com/example/healt4u/screen/componentUI/dataFieldInput.kt
@@ -1,4 +1,46 @@
 package com.example.healt4u.screen.componentUI
 
-class dataFieldInput {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InputtextField(value :String, onValueChange : (String) -> Unit, label : String, modifier: Modifier = Modifier){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {Text(text =label)},
+        singleLine = true,
+        shape = RoundedCornerShape(20.dp),
+
+        colors = TextFieldDefaults.colors(
+
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            disabledContainerColor = Color.White,
+
+            // Indicator colors (underline/border)
+            focusedIndicatorColor = Color(0xFF011792),
+            unfocusedIndicatorColor = Color(0xFF3674EE),
+            disabledIndicatorColor = Color(0xFF3674EE),
+
+
+            // Text colors
+            focusedTextColor = Color(0xFF011792),
+            unfocusedTextColor = Color(0xFF3674EE),
+
+            // Placeholder colors
+            focusedPlaceholderColor = Color(0xFF011792),
+            unfocusedPlaceholderColor = Color(0xFF3674EE)
+        ),
+
+        modifier = modifier.fillMaxWidth()
+    )
+
 }

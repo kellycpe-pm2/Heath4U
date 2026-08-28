79cdd54629b6f6889e91f8873cdb1377b217d3ce
yj <ngyj-pm25@student.tarc.edu.my>
2026-08-28 15:30:54 +0800

    5

--- app/src/main/java/com/example/healt4u/nav/AppNavGraph.kt
+++ app/src/main/java/com/example/healt4u/nav/AppNavGraph.kt
@@ -60,6 +60,7 @@ import com.example.healt4u.model.Message
 import kotlinx.coroutines.launch
 import com.example.healt4u.Storage.getMessagesByConversation
 import com.example.healt4u.Storage.sendMessage
+import com.example.healt4u.screen.appointment.AppointmentScreen
 
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -159,7 +160,8 @@ fun AppNavGraph(
                 onMedicineClick = { navController.navigate("list") },
                 onScheduleClick = { navController.navigate("schedule") },
                 onChatClick = { navController.navigate("chat_list") },
-                onFamilyModeClick = { navController.navigate("family_mode") }
+                onFamilyModeClick = { navController.navigate("family_mode") },
+                onAppointmentClick ={navController.navigate("appointment")}
             )
         }
 
@@ -292,6 +294,16 @@ fun AppNavGraph(
                     }
                 )
             }
+        }
+
+        composable("appointment") {
+            AppointmentScreen(
+                onBack = { navController.popBackStack() },
+                onConfirm = { hospital, doctor, date, time ->
+                    //
+                    navController.popBackStack()
+                }
+            )
         }
 
         composable("chat_list") {

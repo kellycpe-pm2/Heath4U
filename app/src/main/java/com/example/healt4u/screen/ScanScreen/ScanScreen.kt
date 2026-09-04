8ce55ed9b944c9a57d144748bd351063cfe05c80
Kelly Chua Phei Erl <yintaobing@gmail.com>
2026-09-04 19:31:33 +0800

    Back

--- app/src/main/java/com/example/healt4u/screen/ScanScreen/ScanScreen.kt
+++ app/src/main/java/com/example/healt4u/screen/ScanScreen/ScanScreen.kt
@@ -49,7 +49,7 @@ fun ScannerScreen(
                 onGalleryPick =
                     onGalleryPick,
 
-                onBackClick = {}
+                onBackClick = onBackClick
             )
         }
 

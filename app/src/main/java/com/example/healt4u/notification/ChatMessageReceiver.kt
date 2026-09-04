176a48a19cb3b23be28c49e443a0a8c0097e3bdd
yj <ngyj-pm25@student.tarc.edu.my>
2026-09-04 19:01:03 +0800

    29

--- app/src/main/java/com/example/healt4u/notification/ChatMessageReceiver.kt
+++ app/src/main/java/com/example/healt4u/notification/ChatMessageReceiver.kt
@@ -1,10 +1,13 @@
 package com.example.healt4u.notification
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.RequiresPermission
 
 class ChatMessageReceiver : BroadcastReceiver() {
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
     override fun onReceive(context: Context, intent: Intent) {
         val senderName = intent.getStringExtra("senderName") ?: "New Message"
         val messageText = intent.getStringExtra("message") ?: ""
